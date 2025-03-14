bd50bb930f6bed83a88377fdfdd86ba5a85b30cf
Steven <stevenalvarezvillamizar14@gmail.com>
2025-03-14 15:02:53 -0500

    Agregadas cabeceras CORS para Mercado Pago

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,6 @@ const app = express();
 import { corsConfig } from './config/cors';
 app.use(cors(corsConfig));
 
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-
 app.use(express.json());
 
 connectDB();
